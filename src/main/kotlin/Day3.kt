52ea1e4fad974497d90b9afb63ef69caf8493684
GeorgeII <egorzorikov@gmail.com>
2021-12-07 20:35:38 +0400

    Day3 part1

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+object Day3 {
+
+    fun part1(file: String): Int {
+        val reports = File(file)
+            .useLines { it.toList() }
+
+        val mostCommon = (0 until reports[0].length)
+            .map { idx ->
+                val onesNumber = reports
+                    .count { it[idx] == '1' }
+
+                if (onesNumber > reports.size / 2) '1' else '0'
+            }
+            .joinToString("")
+
+        val gammaRate = mostCommon.toInt(2)
+        val epsilon = mostCommon
+            .map { if (it == '1') '0' else '1' }
+            .joinToString("")
+            .toInt(2)
+
+
+        return gammaRate * epsilon
+    }
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,4 +8,7 @@ fun main() {
     val d2Filename = "data/inputDay2"
     println(Day2.part1(d2Filename))
     println(Day2.part2(d2Filename))
+
+    val d3Filename = "data/inputDay3"
+    println(Day3.part1(d3Filename))
 }
