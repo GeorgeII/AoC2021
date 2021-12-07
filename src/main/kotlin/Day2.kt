e0ce41839b45eb7ae38d47be83409ba5e997ef1d
GeorgeII <egorzorikov@gmail.com>
2021-12-07 14:54:22 +0400

    Day2 added

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,45 @@
+import java.io.File
+
+object Day2 {
+
+    data class Position(val horizontal: Int, val vertical: Int)
+    data class AimedPosition(val aim: Int, val horizontal: Int, val vertical: Int)
+
+    fun part1(file: String): Int {
+        val course = File(file)
+            .useLines { it.toList() }
+            .map { it.split(" ") }
+            .map { Pair(it[0], it[1].toInt()) }
+
+        val finalPosition = course
+            .fold(Position(0, 0)) { acc, (command, dist) ->
+                when (command) {
+                    "forward" -> acc.copy(horizontal = acc.horizontal + dist)
+                    "down"    -> acc.copy(vertical = acc.vertical + dist)
+                    "up"      -> acc.copy(vertical = acc.vertical - dist)
+                    else      -> acc
+                }
+            }
+
+        return finalPosition.horizontal * finalPosition.vertical
+    }
+
+    fun part2(file: String): Int {
+        val course = File(file)
+            .useLines { it.toList() }
+            .map { it.split(" ") }
+            .map { Pair(it[0], it[1].toInt()) }
+
+        val finalPosition = course
+            .fold(AimedPosition(0, 0, 0)) { acc, (command, dist) ->
+                when (command) {
+                    "forward" -> acc.copy(horizontal = acc.horizontal + dist, vertical = acc.vertical + acc.aim * dist)
+                    "down"    -> acc.copy(aim = acc.aim + dist)
+                    "up"      -> acc.copy(aim = acc.aim - dist)
+                    else      -> acc
+                }
+            }
+
+        return finalPosition.horizontal * finalPosition.vertical
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,4 +5,7 @@ fun main() {
     println(Day1.part1(d1Filename))
     println(Day1.part2(d1Filename))
 
+    val d2Filename = "data/inputDay2"
+    println(Day2.part1(d2Filename))
+    println(Day2.part2(d2Filename))
 }
